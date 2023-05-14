23b11c26baee0c47306b0b9186fbdb6c8c2d03c9
Robinson Dos Santos <robinsonfdossantos@gmail.com>
2023-05-14 11:32:03 +0930

    Polishing code and creating README.md

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# README.md Creator  ![License](https://img.shields.io/badge/license-MIT-blue.svg)
+# README-Creator  ![License](https://img.shields.io/badge/license-MIT-blue.svg)
 
   ## Description
-  This is a software to create a professional README file, where when answering the questionnaire that will be presented to the end user it will automatically create, organize and style a README file that can be used in their projects.
-    
+  This is a software to create a professional README file, where when asnwering the questionnaire that will be presented to the end user it will autommatically create, organize and style a README file that can be used in their projects.
+  
   ## Table of Contents
   - [Installation](#installation)
   - [Usage](#usage)
@@ -12,31 +12,16 @@
   - [Questions](#questions)
   
   ## Installation
-    Navigate to https://github.com/robinsonfdossantos/README-Creator.
-
-    Open a local built-in Terminal (Powershell, Bash) on your local machine.
-
-    Navigate to a desired folder to place your project in, using 'cd "folder"'.
-
-    Use 'git clone' to copy the repository to your local computer by typing "git clone git@github.com:robinsonfdossantos/README-Creator.git".
-
+  Navigate to  https://github.com/robinsonfdossantos/README-Creator. /n/n Open a local built-in Terminal (powershell, bash) on your local machine. /n/n Navigate to a desired folder to place your project in, using 'cd "folder"'. /n/n Use 'git clone' to copy the repository to your local computer by typing "git clone git@github.com:robinsonfdossantos/README-Creator.git".
+  
   ## Usage
-
-    Run the program using the terminal built into VS Code.
-
-    Answer the questions prompted to complete your README file.
-
-    When answering the questions, if you want to create a new paragraph, instead of typing 'Enter', use the command '/n/n' to go to the next paragraph in the same section.
-
-    If you type 'Enter' by mistake and skip to the next question, don't worry, later you can access your README file and edit it before uploading it to your repository.
-
-    After you finish answering all the questions, a file called README.md will appear for you, so you can copy the file into your personal project.
+  Run the program using the terminal built into VS Code. /n/n Answer the questions prompted to complete your README file. /n/n When answering the questions, if you want to create a new paragraph, insted of typing 'Enter', use the commnad '/n/n' to go to the next paragraph in the same section. /n/n If you type 'Enter' by mistake and skip to the next question, do not worry, later you can access your README file and edit ir before uploading it to your repository. /n/n  After you finish answering all the questions, a file called README.md will apper for you, so you can copy the file into your personal project.
   
   ## License
   This application is covered under the MIT license.
   
   ## Contributing
-  All users are very welcome to leave their comments and suggestions for improvement by sending me a direct message or creating Pull Requests on GitHub.
+  All users are very welcome to leave their comments and suggestions for improvement by sending me a direct message or creating pull requests on Github.
   
   ## Tests
   Run the program, fill in the questions and check the created file against your expectations before uploading it to your repositories.
--- index.js
+++ index.js
@@ -1,61 +1,58 @@
-// TODO: Include packages needed for this application
+// *** Packages needed for this application ***
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-
-// use activity 20
-
-// TODO: Create an array of questions for user input
+// *** Array of questions for user input ***
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: "What is your project title?",
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: "Describe your project in details => ",
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: "Provide installation instructions => ",
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: "Describe the steps of usage of the software => ",
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: "Choose a license for your application",
-        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'EPL', 'CDDL', 'Not Licensed'],
-      },
-      {
-        type: 'input',
-        name: 'contribution',
-        message: "Provide contribution guidelines => ",
-      },
-      {
-        type: 'input',
-        name: 'tests',
-        message: "Describe the tests procedures => ",
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: "Enter your GitHub username => ",
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: "Enter your email address => ",
-      },
+  {
+    type: 'input',
+    name: 'title',
+    message: "What is your project title?",
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: "Describe your project in details => ",
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: "Provide installation instructions => ",
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: "Describe the steps of usage of the software => ",
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: "Choose a license for your application",
+    choices: ['MIT', 'Apache', 'GPL', 'BSD', 'EPL', 'CDDL', 'Not Licensed'],
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: "Provide contribution guidelines => ",
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: "Describe the tests procedures => ",
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: "Enter your GitHub username => ",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Enter your email address => ",
+  },
 ];
 
-// **** Create the README.md *****
+// *** Create the README.md ***
 function createREADME(answers) {
 
   let licenseLogo = '';
@@ -66,7 +63,7 @@ function createREADME(answers) {
     licenseInfo = `This application is covered under the ${answers.license} license.`;
   }
 
-    return `# ${answers.title}  ${licenseLogo}
+  return `# ${answers.title}  ${licenseLogo}
 
   ## Description
   ${answers.description}
@@ -97,22 +94,22 @@ function createREADME(answers) {
   ## Questions
   For any questions or inquiries, please reach out to me via [GitHub](https://github.com/${answers.github}) or email me at ${answers.email}.
   `;
-  }
+}
 
 
-// TODO: Create a function to write README file
+// *** Function to write README file ***
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
+  fs.writeFile(fileName, data, (err) =>
     err ? console.error(err) : console.log('Congratulations!! README.md created :)')
   );
 }
 
-// TODO: Create a function to initialize app
+// *** Function to initialize app ***
 function init() {
-    inquirer.prompt(questions).then((answers) => {
-        const readme = createREADME(answers);
-        writeToFile('README.md', readme);
-      });
+  inquirer.prompt(questions).then((answers) => {
+    const readme = createREADME(answers);
+    writeToFile('README.md', readme);
+  });
 }
 
 // Function call to initialize app
